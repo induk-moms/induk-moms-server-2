cb2c7672db78d66d8bb94e1337279e41c6d32e7f
haebyun <bhk4392@gmail.com>
2024-08-20 04:37:25 +0900

    docs : s3 url 변경

--- src/main/java/induckmoms/server/common/config/WebConfig.java
+++ src/main/java/induckmoms/server/common/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://dentlix-s3.s3-website.ap-northeast-2.amazonaws.com", "http://localhost:3000", "http://192.168.0.11", "http://localhost:5000", "http://192.168.0.11:3000")
+                .allowedOriginPatterns("http://induck-moms-s3.s3-website.ap-northeast-2.amazonaws.com/", "http://localhost:3000", "http://192.168.0.11", "http://localhost:5000", "http://192.168.0.11:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true);
